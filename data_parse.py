e32a4a2d02d077062e0cd8e7f35c522908db500b
Marcin Kowalski <kowalski.marcin02@gmail.com>
2024-11-05 21:20:30 +0100

    team affiliation

--- data_parse.py
+++ data_parse.py
@@ -1,4 +1,68 @@
-
+import pandas as pd
+import json
+import os
+from tqdm import tqdm
+import time
 
 class DataParser:
-    pass
+    
+    def __init__(self, directory: str):
+        self.directory = directory
+        self._data = self._load_data()
+
+    def _load_data(self) -> list:
+        data = list()
+        print('Wczytywanie danych...')
+        for folder_name in tqdm(os.listdir(self.directory)):
+            folder_full_path = os.path.join(self.directory, folder_name)
+            if os.path.isdir(folder_full_path):
+                with open(os.path.join(self.directory, folder_name, 'Labels-GameState.json')) as f:
+                    d = json.load(f)
+                    data.append(d['annotations'])
+        return data
+    
+    
+    def parse_data_to_df(self) -> pd.DataFrame:
+        start = time.time()
+        print('Tworzenie DataFrame...')
+        flat_dict = dict()
+        for annotations in tqdm(self._data):
+            data_image = filter(lambda x: x['supercategory'] == 'object' 
+                                          and x['bbox_pitch_raw'] is not None, annotations)
+            for img in data_image:
+                for key in img:
+                    if isinstance(img[key], dict):
+                        for key2 in img[key]:
+                            if key + '_' + key2 not in flat_dict:
+                                flat_dict[key + '_' + key2] = []
+                            flat_dict[key + '_' + key2].append(img[key][key2])
+                    else:
+                        if key not in flat_dict:
+                            flat_dict[key] = []
+                        flat_dict[key].append(img[key])
+        df = pd.DataFrame(flat_dict, index=flat_dict['id'])
+        df = df.drop(columns=['id'])
+        self._parsed_df = df
+        return df
+
+    def get_shirts_data(self, image_id: str = None) -> pd.DataFrame:
+        if not hasattr(self, '_parsed_df'):
+            raise AttributeError('DataFrame not created yet. Run parse_data_to_df method first.')
+
+        if image_id is not None:
+            df: pd.DataFrame =  self._parsed_df[self._parsed_df['image_id'] == image_id]
+        else:
+            df: pd.DataFrame = self._parsed_df
+        df = df[df['attributes_role'] == 'player']
+        df = df[['image_id', 'bbox_image_x', 'bbox_image_y', 'bbox_image_w', 'bbox_image_h', 'attributes_team']]
+
+        return df
+
+
+    def get_data(self) -> list:
+        return self._data
+
+    def get_parsed_df(self) -> pd.DataFrame:
+        if hasattr(self, '_parsed_df'):
+            return self._parsed_df
+        raise AttributeError('DataFrame not created yet. Run parse_data_to_df method first.')
---
+++ shirt_color.py
@@ -0,0 +1,93 @@
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+import cv2
+
+
+class ShirtColor:
+    def __init__(self, frame: np.ndarray, boxes: list[np.ndarray], true_values: list[int]):
+        self.frame = frame
+        self.boxes = boxes
+        self.true_values = true_values
+        self.crop_factor = 0.25
+        self.colors = list()
+        self.labels = list()
+
+    def set_frame(self, frame: np.ndarray):
+        self.frame = frame
+
+    def set_boxes(self, boxes: list[np.ndarray]):
+        self.boxes = boxes
+
+    def set_crop_factor(self, crop_factor: float):
+        self.crop_factor = crop_factor
+
+    def set_true_values(self, true_values: list[int]):
+        self.true_values = true_values
+
+    def _get_players(self) -> list[np.ndarray]:
+        players = []
+        for bbox in self.boxes:
+            x, y, w, h = map(lambda elem: int(elem), bbox)
+            player = self.frame[y:y + h, x:x + w]
+            players.append(player)
+        return players
+
+    def _get_kits(self) -> list[np.ndarray]:
+        kits = []
+        players = self._get_players()
+        for player in players:
+            kit = player[player.shape[0] // 6:player.shape[0] // 2,
+                  int(player.shape[1] * self.crop_factor):int(player.shape[1] * (1 - self.crop_factor))]
+            kits.append(kit)
+        return kits
+
+    def get_shirt_color(self) -> list[np.float64]:
+        mean_colors: list[int] = []
+        kits = self._get_kits()
+        for kit in kits:
+            im = Image.fromarray(kit)
+            im = im.convert('P', colors=16)
+            im = np.array(im)
+            mean_color = np.mean(im, axis=(0, 1))
+            mean_colors.append(mean_color)
+        self.colors = mean_colors
+        return mean_colors
+
+    def predict_team(self) -> np.array:
+        if len(self.colors) == 0:
+            return
+        color_values = np.array(self.colors).reshape(-1, 1)
+        kmeans = KMeans(n_clusters=2, random_state=0).fit(color_values)
+        self.labels = kmeans.labels_
+        return kmeans.labels_
+
+    def get_accuracy(self) -> np.float64 | None:
+        if len(self.labels) == 0:
+            return
+        return np.mean(self.labels == self.true_values)
+
+    def run_prediction(self) -> np.float64 | None:
+        self.get_shirt_color()
+        self.predict_team()
+        return self.get_accuracy()
+
+    def plot(self, c: list, return_plot: bool = False):
+        plt.scatter(range(1, len(self.colors) + 1), self.colors, c=c)
+        plt.xlabel('Player')
+        plt.ylabel('Mean color')
+        plt.legend(['Team1', 'Team2'])
+        if not return_plot:
+            plt.show()
+
+    def plot_predicted_colors(self, return_plot: bool = False):
+        if len(self.colors) == 0 or len(self.labels) == 0:
+            return
+        self.plot(self.labels, return_plot)
+
+
+    def plot_true_colors(self, return_plot: bool = False):
+        if len(self.colors) == 0:
+            return
+        self.plot(self.true_values, return_plot)
