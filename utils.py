9062932fb3c48cd2832c1dd8c7920462cefb2d25
Marcin Kowalski <kowalski.marcin02@gmail.com>
2024-11-21 23:14:54 +0100

    getting data for offline analisis

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-def load_frames(source_path: str) -> list[np.ndarray]:
+def load_frames_from_video(source_path: str) -> list[np.ndarray]:
     frames: list[np.ndarray] = list()
     cap = cv2.VideoCapture(source_path)
     while cap.isOpened():
@@ -10,4 +10,13 @@ def load_frames(source_path: str) -> list[np.ndarray]:
             break
         frames.append(frame)
     cap.release()
+    return frames
+
+def load_frames_from_images(source_path: str) -> list[np.ndarray]:
+    frames: list[np.ndarray] = list()
+    for i in range(750):
+        frame = cv2.imread(f'{source_path}/{i+1:06}.jpg')
+        if frame is None:
+            break
+        frames.append(frame)
     return frames
